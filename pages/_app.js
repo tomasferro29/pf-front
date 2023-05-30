be6e29ceba40f8ddf809dd8041fe2d77fdb0c229
Tomas Ferro <tomasferro29@gmail.com>
2023-05-30 11:40:35 -0300

    route account

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { CartContextProvider } from "@/components/CartContext"
 import { createGlobalStyle } from "styled-components"
+import { SessionProvider } from 'next-auth/react'
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
@@ -10,14 +11,15 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'Roboto', sans-serif;
   }
 `
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: {session, ...pageProps} }) {
   return (
     <>
       <GlobalStyles/>
-      <CartContextProvider>
-        <Component {...pageProps} />
-      </CartContextProvider>
-
+      <SessionProvider>
+        <CartContextProvider>
+          <Component {...pageProps} />
+        </CartContextProvider>
+      </SessionProvider>
     </>
   )
 }
